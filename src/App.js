d4e2b48190db836fec37f4b6f4334d594a12046d
Jonathan Hunsucker <jonathantrevorh@gmail.com>
2020-07-26 13:28:43 -0400

    Add safety around destructive actions, statuses, and pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useReducer, useEffect } from 'react'
 import './App.css'
+import { TODO, DONE } from './status'
 
 function App() {
   const [storedState, setStoredState] = useLocalStorage({})
@@ -12,12 +13,26 @@ function App() {
 
   const {
     items = [],
+    page = {
+      id: 'INDEX',
+      params: {},
+    },
   } = state
 
   return (
     <div className="App">
-      {items.map((item) => (<Item key={item.id} item={item} dispatch={dispatch} />))}
-      <button onClick={() => dispatch({ type: 'ADD_ITEM' })}>add</button>
+      {page.id === 'INDEX' && <Index items={items} dispatch={dispatch} />}
+      {page.id === 'ITEM' && <Item item={items.filter((item) => item.id === page.params.itemId)[0]} dispatch={dispatch} />}
+    </div>
+  )
+}
+
+function Index({ items, dispatch }) {
+  return (
+    <div>
+      <button onClick={() => dispatch({ type: 'ADD_ITEM' })}>new</button>
+      {items.filter((item) => item.status !== DONE).map((item) => (<ItemLink key={item.id} item={item} dispatch={dispatch} />))}
+      {items.filter((item) => item.status === DONE).map((item) => (<ItemLink key={item.id} item={item} dispatch={dispatch} />))}
     </div>
   )
 }
@@ -25,8 +40,29 @@ function App() {
 function Item({ item, dispatch }) {
   return (
     <div>
+      <button onClick={() => dispatch({ type: 'GOTO', page: 'INDEX' })}>back</button>
+      {item && <ItemEditor item={item} dispatch={dispatch} />}
+    </div>
+  )
+}
+
+function ItemLink({ item, dispatch }) {
+  return (
+    <div>
+      <ButtonLink style={{ textDecoration: item.status === DONE ? 'strikethrough': 'none' }} onClick={() => dispatch({ type: 'GOTO', page: 'ITEM', params: { itemId: item.id } })}>{item.title}</ButtonLink>
+    </div>
+  )
+}
+
+function ItemEditor({ item, dispatch }) {
+  return (
+    <div>
+      <div>
+        {item.status !== DONE && <button onClick={() => dispatch({ type: 'MARK_AS', itemId: item.id, status: DONE })}>mark as done</button>}
+        {item.status === DONE && <button onClick={() => dispatch({ type: 'MARK_AS', itemId: item.id, status: TODO })}>mark as todo</button>}
+        <button onClick={() => window.confirm('Are you sure?') && dispatch({ type: 'REMOVE_ITEM', itemId: item.id })}>delete...</button>
+      </div>
       <input type="text" value={item.title} onChange={(e) => dispatch({ type: 'SET_ITEM_TITLE', itemId: item.id, value: e.target.value })} />
-      <button onClick={() => dispatch({ type: 'REMOVE_ITEM', itemId: item.id })}>remove</button>
     </div>
   )
 }
@@ -46,7 +82,7 @@ function useLocalStorage(initialValue) {
 const actions = {
   ADD_ITEM: (state) => ({
     ...state,
-    items: [...(state.items || []), { id: generateRandomId(), title: '' }],
+    items: [...(state.items || []), { id: generateRandomId(), title: 'untitled' }],
   }),
   SET_ITEM_TITLE: (state, { itemId, value }) => ({
     ...state,
@@ -55,6 +91,14 @@ const actions = {
   REMOVE_ITEM: (state, { itemId }) => ({
     ...state,
     items: state.items.filter((item) => item.id !== itemId),
+  }),
+  MARK_AS: (state, { itemId, status }) => ({
+    ...state,
+    items: state.items.map((item) => item.id === itemId ? { ...item, status } : item),
+  }),
+  GOTO: (state, { page, params }) => ({
+    ...state,
+    page: { id: page, params },
   }),
 }
 
@@ -68,6 +112,12 @@ function reducer(state, action) {
 
 function generateRandomId() {
   return Math.random().toString(36).substring(7)
+}
+
+function ButtonLink({ onClick, children }) {
+  return (
+    <button style={{ fontFamily: 'serif', border: '0', fontSize: '1em', background: 'transparent', margin: '0', padding: '0', textDecoration: 'underline', color: 'blue' }} onClick={onClick}>{children}</button>
+  )
 }
 
 export default App
